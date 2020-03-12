fa9348807c8b0e53efb95f3b4692ffb19fcde631
GuilhermeSousaBorges <62119980+GuilhermeSousaBorges@users.noreply.github.com>
2020-03-12 19:00:03 -0300

    Add files via upload

---
+++ qest5.m
@@ -0,0 +1,19 @@
+function a = ErroRelativo(x1, x2)
+
+
+if(x2==0)
+  a = x1
+else
+  a = (x1-x2)/x2
+endif
+endfunction
+
+function raiz = raizquadrada(a, b, c, d)
+  
+  k = 1
+  eRel = +
+  
+  
+  
+  
+endfunction
